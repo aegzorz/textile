640bdb69639937df014623074d257d42fe9d6d97
Alexander Persson <alexander.persson@sbab.se>
2020-12-20 12:15:34 +0100

    Added specific tests for generating images for Readme

--- Textile/Tests/TextileLabelTests.swift
+++ Textile/Tests/TextileLabelTests.swift
@@ -9,7 +9,6 @@
 import XCTest
 import Textile
 import SnapshotTesting
-import SnapKit
 
 class TextileLabelTests: XCTestCase {
     func testSimpleStyle() {
@@ -35,31 +34,6 @@ class TextileLabelTests: XCTestCase {
         label.textStyle = .combining(.italicRedBackground, .boldBlueForeground)
         
         // Should be blue bold on red background
-        assertSnapshot(matching: label, as: .image)
-    }
-    
-    func testCustomFont() {
-        let font = UIFont(font: TextileTestsFontFamily.Quicksand.dash, size: 72)
-        
-        let style = TextStyle { style in
-            style.set(.font, font)
-            style.set(.foregroundColor, .red)
-            style.set(.baselineOffset, -4)
-            style.set(.kern, 3)
-            
-            let shadow = NSShadow()
-            shadow.shadowColor = UIColor(white: 0, alpha: 0.3)
-            shadow.shadowOffset = CGSize(width: 0.5, height: 0.5)
-            shadow.shadowBlurRadius = 1
-            
-            style.set(.shadow, shadow)
-        }
-        
-        let label = TextileLabel(style: style, text: " Textile ")
-        
-        // <a href="https://www.freepik.com/photos/background">Background photo created by rawpixel.com - www.freepik.com</a>
-        label.backgroundColor = UIColor(patternImage: TextileTestsAsset.whiteMicrofiberFabricBackground.image)
-                
         assertSnapshot(matching: label, as: .image)
     }
 }

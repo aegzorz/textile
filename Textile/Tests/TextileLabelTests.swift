d883bede42a20110a14ea5dbcd43bdccb73492f7
Alexander Persson <aegzorz@gmail.com>
2020-12-20 17:07:14 +0100

    Added test utilizing label configurator

--- Textile/Tests/TextileLabelTests.swift
+++ Textile/Tests/TextileLabelTests.swift
@@ -36,4 +36,13 @@ class TextileLabelTests: XCTestCase {
         // Should be blue bold on red background
         assertSnapshot(matching: label, as: .image)
     }
+    
+    func testConfigurator() {
+        let label = TextileLabel(
+            style: .combining(.simple, .multiline),
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )
+        
+        assertSnapshot(matching: label, as: .image(size: CGSize(width: 350, height: 260)))
+    }
 }
